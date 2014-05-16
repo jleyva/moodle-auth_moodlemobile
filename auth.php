8676b6669e8ac920124fec2b47684de7a73442fa
Juan Leyva <juanleyvadelgado@gmail.com>
2014-05-16 11:02:04 +0200

    Base plugin without extra functionality

---
+++ auth.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Moodle Mobile auth plugin, this plugin allows the Mobile app to check different auth configurations.
+ *
+ * @package    auth_moodlemobile
+ * @copyright  2014 Juan Leyva <juanleyvadelgado@gmail.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+require_once($CFG->libdir . '/authlib.php');
+
+/**
+ * Moodle Mobile auth plugin.
+ */
+class auth_plugin_moodlemobile extends auth_plugin_base {
+
+    /**
+     * Constructor.
+     */
+    public function __construct() {
+        $this->authtype = 'moodlemobile';
+        $this->config = get_config('auth/moodlemobile');
+    }
+
+    /**
+     * We allways return false since this plugins cannot be used by a user.
+     *
+     * @param string $username The username (with system magic quotes)
+     * @param string $password The password (with system magic quotes)
+     *
+     * @return bool Authentication success or failure.
+     */
+    public function user_login($username, $password) {
+        return false;
+    }
+
+    /**
+     * Returns true if this authentication plugin can edit the users'
+     * profile.
+     *
+     * @return bool
+     */
+    public function can_edit_profile() {
+        return false;
+    }
+
+    /**
+     * Returns true if this authentication plugin is 'internal'.
+     *
+     * @return bool
+     */
+    public function is_internal() {
+        return false;
+    }
+
+    /**
+     * Prints a form for configuring this authentication plugin.
+     *
+     * This function is called from admin/auth.php, and outputs a full page with
+     * a form for configuring this plugin.
+     *
+     * @param array $page An object containing all the data for this page.
+     */
+    public function config_form($config, $err, $userfields) {
+    }
+
+    /**
+     * Processes and stores configuration data for this authentication plugin.
+     */
+    public function process_config($config) {
+        return true;
+    }
+
+    /**
+     * Confirm the new user as registered. This should normally not be used,
+     * but it may be necessary if the user auth_method is changed to manual
+     * before the user is confirmed.
+     */
+    public function user_confirm($username, $confirmsecret = null) {
+        return AUTH_CONFIRM_ERROR;
+    }
+
+}
---
+++ lang/en/auth_moodlemobile.php
@@ -0,0 +1,26 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'auth_moodlemobile', language 'en'.
+ *
+ * @package   auth_moodlemobile
+ * @copyright 2014 Juan Leyva <juanleyvadelgado@gmail.com>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['auth_moodlemobiledescription'] = 'Authentication plugin used by the Mobile app to check if the Mobile service is enabled and if the site uses SSO plugins.';
+$string['pluginname'] = 'Moodle Mobile authentication';
